ebb630a8c1f718a033db0ca06d0482f27ca980e3
tnielsontech <casadotiagonielson@gmail.com>
2025-06-17 00:35:22 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 export default async function handler(req, res) {
-  const url = 'https://script.google.com/macros/s/AKfycbwUkYq3OGh_DB7ovV923MyXINmKKZM0fX8rChCs8T7U3lJn9YFbAsXW_PyPWDuJCgI/exec' + req.url;
+  const url = 'https://script.google.com/macros/s/AKfycbymvlZV1ffXO9w_Q71Rn4LW8b8kVdFsXhs8gdSdwMDtNxwGKhS8_ECMBpp8oaZXAdY/exec' + req.url;
 
   const response = await fetch(url, {
     method: req.method,
