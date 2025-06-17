4a32d6ec3fda8115c28c7fd6dc67ac8a9ca948bb
tnielsontech <casadotiagonielson@gmail.com>
2025-06-17 09:11:35 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,8 +1,7 @@
 export default async function handler(req, res) {
   const baseURL = "https://script.google.com/macros/s/AKfycbymvlZV1ffXO9w_Q71Rn4LW8b8kVdFsXhs8gdSdwMDtNxwGKhS8_ECMBpp8oaZXAdY/exec";
-
-  const queryString = req.url.includes("?") ? req.url.substring(req.url.indexOf("?")) : "";
-  const url = `${baseURL}${queryString}`;
+  const queryString = req.url.split('?')[1] || '';
+  const url = `${baseURL}?${queryString}`;
 
   const fetchOptions = {
     method: req.method,
@@ -20,11 +19,11 @@ export default async function handler(req, res) {
     const text = await response.text();
 
     res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
     res.status(200).send(text);
   } catch (error) {
-    res.status(500).json({ error: "Erro ao conectar com o Apps Script" });
+    res.status(500).send("Erro no Proxy: " + error.toString());
   }
 }
