8f01208b7fb57cab39928bfafb93a3b482c90e13
tnielsontech <casadotiagonielson@gmail.com>
2025-06-17 08:05:37 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -2,12 +2,16 @@ export default async function handler(req, res) {
   const url = "https://script.google.com/macros/s/AKfycbymvlZV1ffXO9w_Q71Rn4LW8b8kVdFsXhs8gdSdwMDtNxwGKhS8_ECMBpp8oaZXAdY/exec" 
     + (req.url.includes("?") ? req.url.substring(req.url.indexOf("?")) : "");
 
-  const response = await fetch(url);
-  const data = await response.json();
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
 
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
-  res.status(200).json(data);
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error: "Erro ao conectar com o Apps Script" });
+  }
 }
